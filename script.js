8d4e7eb7bed08d4c0a41a3906ea557c43063aef9
Balramrawat <110815042+Balramrawat@users.noreply.github.com>
2025-02-07 19:46:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+// let colorselect=document.querySelector("#colorSelect")
+let inp=document.querySelector("input")
+
+inp.addEventListener("click",()=>{
+	let colorselect=document.querySelector("#colorSelect")
+	let index=colorselect.selectIndex;
+	if(index !== -1){
+		colorselect.remove(index)
+	}
+})
